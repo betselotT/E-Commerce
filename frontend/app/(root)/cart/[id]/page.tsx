0f55b9c3c05028f6b95f120caa1feba84d94b6f2
betselotT <betselottesfa9@gmail.com>
2025-09-08 10:11:56 +0300

    fixed build error

--- frontend/app/(root)/cart/[id]/page.tsx
+++ frontend/app/(root)/cart/[id]/page.tsx
@@ -156,7 +156,7 @@ const CartPage = () => {
                 Your cart is empty
               </h3>
               <p className="text-gray-600">
-                Looks like you haven't added anything to your cart yet.
+                Looks like you haven&apos;t added anything to your cart yet.
               </p>
             </div>
           ) : (
